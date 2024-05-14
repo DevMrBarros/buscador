6e372b7c2f30a95239d19d56f80b300193d4d017
DevMrBarros <rafaelbarrosmauricio@gmail.com>
2024-05-14 11:59:44 -0300

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"; // Importa a função useState do React
+import { FiSearch } from "react-icons/fi"; // Importa o ícone de busca do React Icons
+import "./style.css"; // Importa o arquivo de estilos CSS
+
+import api from "./services/api"; // Importa o módulo de API
 
 function App() {
+  // Declaração de estados utilizando useState
+  const [input, setInput] = useState(""); // Estado para o valor do input
+  const [cep, setCep] = useState({}); // Estado para armazenar dados do CEP
+
+  // Função assíncrona para lidar com a busca do CEP
+  async function handleSearch() {
+    if (input === '') {
+      alert("Preencha algum CEP!"); // Alerta se o input estiver vazio
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`); // Faz a requisição à API
+      setCep(response.data); // Atualiza o estado do CEP com os dados da resposta
+      setInput(""); // Limpa o input após a busca
+
+    } catch {
+      alert("Ops, erro ao buscar cep!"); // Alerta em caso de erro na requisição
+      setInput(""); // Limpa o input em caso de erro
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"> {/* Contêiner principal */}
+      <h1 className="title">Buscador CEP</h1> {/* Título da página */}
+
+      {/* Contêiner para entrada de dados */}
+      <div className="container-input">
+        <input 
+          type="text" 
+          placeholder="Digite seu cep..." 
+          value={input} 
+          onChange={(e) => setInput(e.target.value)} 
+        /> {/* Input controlado que atualiza o estado 'input' */}
+
+        {/* Botão de busca com ícone */}
+        <button className="buttonSearch" onClick={handleSearch}>
+         <FiSearch size={25} color="#FFF" /> {/* Ícone de busca */}
+        </button>
+      </div>
+
+      {/* Condição para renderizar os dados do CEP */}
+      {Object.keys(cep).length > 0 && (
+        <main className="main"> {/* Elemento principal */}
+          <h2>CEP: {cep.cep}</h2> {/* Exibe o CEP retornado pela API */}
+
+          <span>{cep.logradouro}</span> {/* Exibe o logradouro */}
+          <span>Complemento: {cep.complemento}</span> {/* Exibe o complemento */}
+          <span>{cep.bairro}</span> {/* Exibe o bairro */}
+          <span>{cep.localidade} - {cep.uf}</span> {/* Exibe a localidade e UF */}
+        </main>
+      )};
+
     </div>
   );
 }
 
-export default App;
+export default App; // Exporta o componente App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // Importa a biblioteca React
+import ReactDOM from 'react-dom/client'; // Importa a função ReactDOM para renderização
+import './index.css'; // Importa o arquivo de estilos CSS
+import App from './App'; // Importa o componente principal App
 
+// Cria um root para renderização utilizando ReactDOM
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderiza o componente App dentro do root, com modo estrito (StrictMode) do React
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
